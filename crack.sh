569af4627e05ca0467dd62c1488b4f19e1c88f85
CHANDAN727 <62067152+CHANDAN727@users.noreply.github.com>
2020-04-22 12:20:38 +0530

    Add files via upload

---
+++ crack.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+clear
+echo -e "\e[4;31m @Harry !!! \e[0m"
+echo -e "\e[1;34m Presents \e[0m"
+echo -e "\e[1;32m Cracker \e[0m"
+echo "Press Enter To Continue"
+read a1
+if [[ -s update.Harry ]];then
+echo "All Requirements Found...."
+else
+echo 'Installing Requirements....'
+echo .
+echo .
+apt install figlet toilet python curl -y
+apt install python3-pip
+pip install -r requirements.txt
+echo This Script Was Made By Harry >update.Harry
+echo Requirements Installed....
+echo Press Enter To Continue...
+read upd
+fi
+while :
+do
+rm *.xxx >/dev/null 2>&1
+clear
+echo -e "\e[1;31m"
+figlet TBomb
+
+
+
+echo " "
+echo -e "\e[4;31m Please Read Instruction Carefully !!! \e[0m"
+echo " "
+echo "Press 1 To  Start SMS Bomber "
+echo "Press 2 To  Start Call Bomber "
+echo "Press 3 To  Update (Works On Linux And Linux Emulators) "
+echo "Press 4 To  View Features "
+echo "Press 5 To  Exit "
+read ch
+if [ $ch -eq 1 ];then
+clear
+echo -e "\e[1;32m"
+rm *.xxx >/dev/null 2>&1
+python3 bomber.py
+rm *.xxx >/dev/null 2>&1
+exit 0
+elif [ $ch -eq 2 ];then
+clear
+echo -e "\e[1;32m"
+echo 'Call Bomb By Harry'> call.xxx
+python3 bomber.py call
+rm *.xxx >/dev/null 2>&1
+exit 0
+elif [ $ch -eq 3 ];then
+clear
+apt install git -y
+echo -e "\e[1;34m Downloading Latest Files..."
+git clone https://github.com/TheHarry/TBomb
+if [[ -s TBomb/TBomb.sh ]];then
+cd TBomb
+cp -r -f * .. > temp
+cd ..
+rm -rf  TBomb >> temp
+rm update.Harry >> temp
+rm temp
+chmod +x TBomb.sh
+fi
+echo -e "\e[1;32m TBomb Will Restart Now..."
+echo -e "\e[1;32m All The Required Packages Will Be Installed..."
+echo -e "\e[1;34m Press Enter To Proceed To Restart..."
+read a6
+./TBomb.sh
+exit
+elif [ $ch -eq 4 ];then
+clear
+echo -e "\e[1;33m"
+figlet TBomb
+
+echo "Press Enter To Go Home"
+read a3
+clear
+elif [ $ch -eq 5 ];then
+clear
+echo -e "\e[1;31m"
+figlet TBomb
+
+exit 0
+else
+echo -e "\e[4;32m Invalid Input !!! \e[0m"
+echo "Press Enter To Go Home"
+read a3
+clear
+fi
+done
